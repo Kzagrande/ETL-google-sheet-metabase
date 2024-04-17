f0eb3f6a55950437301c4955d7c47ff97d37f6e3
BortoletoYan <138122033+BortoletoYan@users.noreply.github.com>
2024-04-17 19:42:59 -0300

    fix_code

--- logs.py
+++ logs.py
@@ -1,5 +1,6 @@
 import os 
 
 def log(mensagem):
-    os.getenv("PROD_ENV")
+    env = os.getenv("PROD_ENV")
+    print("O valor da variável de ambiente MINHA_VARIAVEL é:", env)
     print('log event', mensagem)
